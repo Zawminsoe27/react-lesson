ea8bb3e11936536a3675a637589429ff9334cd86
zawminsoe <alinyaung225800@gmail.com>
2023-11-21 06:12:58 +0630

    style components in react

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,30 +1,23 @@
 import React from "react";
-import "./App.css";
-import AnimalCard from "../AnimalCard/Animalcard";
-import data from "./data";
-// function showAdditional(additional) {
-// 	const alertInformation = Object.entries(additional)
-// 		.map((information) => `${information[0]}: ${information[1]}`)
-// 		.join("\n");
-// 	alert(alertInformation)
-// }
+import { createUseStyles } from "react-jss";
+import Alert from "../Alert/Alert";
+import CartSuccess from "../CartSuccess/Cartsuccess";
+
+let useStyles = createUseStyles({
+	wrapper: {
+		padding: 20,
+	},
+});
+
 function App() {
+	const classes = useStyles();
 	return (
-		<div className="wrapper">
-			<h1>Animals</h1>
-			{data.map((animal) => (
-				<AnimalCard
-					// additional={animal.additional}
-					diet={animal.diet}
-					key={animal.name}
-					name={animal.name}
-					size={animal.size}
-					scientificName={animal.scientificName}
-					// showAdditional={showAdditional}
-				/>
-			))}
+		<div className={classes.wrapper}>
+			<Alert title="Items Not Added" type="error">
+				<div>Your items are out of stock</div>
+			</Alert>
+			<CartSuccess />
 		</div>
 	);
 }
-
 export default App;
